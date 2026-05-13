073f48f8738396a89c7eff1da9aa8cdf66428273
Stephen Chryn <srcthird@gmail.com>
2026-05-13 00:17:45 -0400

    Update depricated ModLoadingContext

--- src/main/java/me/frigidambiance/customhotbar/CustomHotbar.java
+++ src/main/java/me/frigidambiance/customhotbar/CustomHotbar.java
@@ -1,15 +1,15 @@
 package me.frigidambiance.customhotbar;
 
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(CustomHotbar.MOD_ID)
 public class CustomHotbar {
     public static final String MOD_ID = "customhotbar";
 
-    public CustomHotbar() {
-        ModLoadingContext.get().registerConfig(
+    public CustomHotbar(FMLJavaModLoadingContext context) {
+        context.registerConfig(
                 ModConfig.Type.CLIENT,
                 Config.SPEC
         );
